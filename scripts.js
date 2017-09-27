de7046fea2682ff995f21a64448351af9f8999a2
Soleil Miller <Soleil@172-18-184-125.DYNAPOOL.NYU.EDU>
2017-09-26 20:05:47 -0400

    homework 4

--- scripts.js
+++ scripts.js
@@ -12,3 +12,16 @@ for (let i=1 ; i<= input; i = i + 1){
     $("#response").append(i + " is odd.<br>");
   }
 }
+let tipCalculator;
+tipCalculator = function(total, tipRate){
+  // step 1:
+  let tipAmount;
+  tipAmount = tipRate * total;
+  // and step 2:
+  $("#response").html("Your tip is $" + tipAmount);
+}
+
+// Now call (or “execute”) the function, passing a
+// total of $50.00 and a tipRate of 20%:
+
+tipCalculator(50.00, 0.2);
