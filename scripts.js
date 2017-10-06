677efe852b10f7db93bc30f094215500c8b9b7c2
Soleil Miller <Soleil@172-18-187-193.DYNAPOOL.NYU.EDU>
2017-10-06 18:59:14 -0400

    homework 4

--- scripts.js
+++ scripts.js
@@ -1,27 +1,18 @@
-let input;
-// asks for a number
-input = prompt("Please enter a number");
-// prints all the numbers from 1 to it.
-
-$("#response").html("");
-
-for (let i=1 ; i<= input; i = i + 1){
-  if (i % 2 === 0){
-      $("#response").append(i + " is even.<br>");
-  } else {
-    $("#response").append(i + " is odd.<br>");
-  }
-}
-let tipCalculator;
-tipCalculator = function(total, tipRate){
-  // step 1:
-  let tipAmount;
-  tipAmount = tipRate * total;
-  // and step 2:
-  $("#response").html("Your tip is $" + tipAmount);
+.map {
+  /* Leaflet *requires* a height to be explictly set for the map object. */
+  height: 300px;
 }
 
-// Now call (or “execute”) the function, passing a
-// total of $50.00 and a tipRate of 20%:
+@import url("https://fonts.googleapis.com/css?family=EB+Garamond");
 
-tipCalculator(50.00, 0.2);
+body {
+  font-family: "EB Garamond", serif;
+}
+
+.leaflet-container {
+  font-family: "EB Garamond", serif;
+}
+
+body, .leaflet-popup-content-wrapper, .leaflet-popup-tip {
+  background-color: #fdf6e3;
+}
